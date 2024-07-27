6f86cf27e3f15cb569495678c461163535de92d7
jcontan <111036786+jcontan@users.noreply.github.com>
2024-07-27 17:56:51 -0400

    Update color3.js

--- color3.js
+++ color3.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.getElementById('flashing-header');
+    const colors = ['magenta', 'yellow', 'black', 'red', '#0FFF00', 'white', 'cyan'];
+    let index = 0;
 
+    function changeColor() {
+        header.style.color = colors[index];
+        index = (index + 1) % colors.length;
+    }
+
+    setInterval(changeColor, 1000);
+});
