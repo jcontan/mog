11277af9bce98096aa9b8de47c6843449ecc538f
jcontan <111036786+jcontan@users.noreply.github.com>
2024-07-27 17:55:49 -0400

    Create color2.js

---
+++ color2.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.getElementById('flashing-header');
+    const colors = ['white', 'cyan', 'magenta', 'yellow', 'black', 'red', '#0FFF00', ];
+    let index = 0;
+
+    function changeColor() {
+        header.style.color = colors[index];
+        index = (index + 1) % colors.length;
+    }
+
+    setInterval(changeColor, 1000);
+});
