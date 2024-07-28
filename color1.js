e7c176cf74ce886d638d389f78df7c7311442b8e
jcontan <111036786+jcontan@users.noreply.github.com>
2024-07-27 23:01:26 -0400

    Update color1.js

--- color1.js
+++ color1.js
@@ -8,5 +8,5 @@ document.addEventListener('DOMContentLoaded', function() {
         index = (index + 1) % colors.length;
     }
 
-    setInterval(changeColor, 1000);
+    setInterval(changeColor, 500);
 });
